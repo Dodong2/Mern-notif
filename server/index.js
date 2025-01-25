1ce2efb7851f910e1f1764637909956644f8f4ad
Dodong2 <carlseighartaliodegmail.com>
2025-01-25 13:49:21 +0800

    2nd commit

--- server/index.js
+++ server/index.js
@@ -1,18 +1,31 @@
 // notif123
-const express = require('express')
-const app = express()
+const express = require('express');
+const app = express();
 
-const mongoose = require('mongoose')
-mongoose.connect('mongodb+srv://carldong:notif123@cluster0.642xs.mongodb.net/notif?retryWrites=true&w=majority&appName=Cluster0')
+const mongoose = require('mongoose');
+const cors = require('cors');
 
-const cors = require('cors')
+// Use environment variables or defaults
+const PORT = process.env.PORT || 3003;
+const MONGO_URI = process.env.MONGO_URI || 'mongodb+srv://carldong:notif123@cluster0.642xs.mongodb.net/notif?retryWrites=true&w=majority&appName=Cluster0';
 
-app.use(express.json())
-app.use(cors())
-
-const UserRoutes = require('./routes/Userlist')
-app.use('/users', UserRoutes)
-
-app.listen(3003, () => {
-    console.log('server running: 3003' )
+// Connect to MongoDB
+mongoose.connect(MONGO_URI, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
 })
+    .then(() => console.log('Connected to MongoDB'))
+    .catch((error) => console.error('Error connecting to MongoDB:', error));
+
+// Middleware
+app.use(express.json());
+app.use(cors());
+
+// Routes
+const UserRoutes = require('./routes/Userlist');
+app.use('/users', UserRoutes);
+
+// Start the server
+app.listen(PORT, () => {
+    console.log(`Server running on port: ${PORT}`);
+});
